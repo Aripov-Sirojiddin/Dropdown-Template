b7ecf6b98330afc6909a7599976b40e12026ee12
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-06-09 12:46:51 +0500

    Dropdown function description and outline placed.

---
+++ dropdown_maker.js
@@ -0,0 +1,17 @@
+/*
+@description Creates a button, which when hovered over reveals the options in a list format.
+@param name : String - name of the button
+@param options : An Object - key is the label of the option, value is the callback function
+@param classes : Array of Strings - an array of any additional classes to be applied to the button. 
+@returns div : HTML element - returns the div container and the children. Sets the ID of div to `div-${@param name}`
+*/
+const dropdown = function(name, options, classes = []) {
+  const make = function() {
+    const container = document.createElement("div");
+
+    
+  }
+  return {
+    make
+  }
+}
