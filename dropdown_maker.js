c89447983c74fbda4d0387d146b4efe8cb323b92
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-06-09 13:14:46 +0500

    Created a makeOptionsDiv which creates a div of all options. Semicolons are being added on their own I am not going crazy.

--- dropdown_maker.js
+++ dropdown_maker.js
@@ -1,37 +1,63 @@
-const dropdown = function (name, options, classes = []) {
+  /*
+  @param name : String - Name of the button.
+  @param options : An Object - key is the label of the option, value is the callback function.
+  @param optionsClassName : String - A class to add to all options button for styling. 
+  @param classes : Array of Strings - an array of any additional classes to be applied to the button. 
+  */
+const dropdown = function (name, options, optionsClassName = "", classes = []) {
   /*
   @description Creates a div container, which holds button and options div.
-  @param name : String - name of the button
-  @param options : An Object - key is the label of the option, value is the callback function
-  @param classes : Array of Strings - an array of any additional classes to be applied to the button. 
   @returns div : HTML element - returns the div container and the children. Sets the ID of div to `div-${@param name}`
   */
   const make = function () {
     const container = document.createElement("div");
     container.id = `div-${name}`;
-  }
+  };
 
   /*
+  Helper function for make();
   @description Creates a button with {@param name} which when hovered over reveals the options in a list format.
-  @param name : String - name of the button;
   @param action : Function - A callback function to do when button is clicked.
   @returns button : HTML element. 
   */
-  const makeButton = function (name, action, classes) {
+  const makeButton = function (action) {
     const button = document.createElement("button");
     button.textContent = name;
 
     //Set click event to trigger action callback function.
-    button.addEventListener("mouseover", () => action);
-    
-    classes.forEach(additional_class => {
+    button.addEventListener("mouseover", action);
+
+    classes.forEach((additional_class) => {
       button.classList.add(additional_class);
     });
 
     return button;
-  }
+  };
+
+  /*
+  Helper function for make();
+  @description Creates a div holding all options.
+  @param options : An Object - key is the label of the option, value is the callback function.
+  @returns div : HTML element - containing all options.
+  */
+  const makeOptionsDiv = function () {
+    const optionsContainer = document.createElement("div");
+    optionsContainer.classList.add("hidden");
+    
+    for (let optionName in options) {
+      const callbackFunction = options[optionName];
+
+      const button = document.createElement("button");
+      button.textContent = optionName;
+      button.classList.add(optionsClassName);
+
+      button.addEventListener("click", callbackFunction);
+      optionsContainer.appendChild(button);
+    }
+    return optionsContainer;
+  };
 
   return {
     make,
-  }
-}
+  };
+};
